b5ff87e01c3df194dafe157d3e22f3e088822d1a
Jesus Mejia <jesusmejded@gmail.com>
2023-03-30 13:40:35 -0600

    minor update

--- src/parameters.jl
+++ src/parameters.jl
@@ -7,12 +7,3 @@ function parameters(ps::Pair...)
     MixedSpace(ps...)
 end
 
-"""
-    ..(a, b)
-
-Define a interval between a and b (inclusive).
-
-See also [`BoxConstrainedSpace`](@ref)
-"""
-..(a::Real, b::Real) = BoxConstrainedSpace(a, b)
-..(a::Bool, b::Bool) = BitArraySpace(1)
