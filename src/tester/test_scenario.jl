7b25bccefaf3d98311cfc450ee6f15d6cb7abad6
Jesus Mejia <jesusmejded@gmail.com>
2023-06-23 15:32:26 -0600

    fix issue related to bcapsampler

--- src/tester/test_scenario.jl
+++ src/tester/test_scenario.jl
@@ -3,13 +3,13 @@ function check_scenario(f::Function, scenario::Scenario; verbose=true)
     try
         trials = sample(scenario::Scenario)
     catch e
-        @error "Parami: I cannot sample trials due to:"
+        @error "HyperTuning: I cannot sample trials due to:"
         println("Error: ", e)
         return false
     end
 
     if isempty(trials)
-        @error "Parami: I cannot sample trials."
+        @error "HyperTuning: I cannot sample trials."
         println("Check sampler o parameters config.")
         println("Provided parameters: ", scenario.parameters)
         println("Provided Sampler: ", scenario.sampler)
@@ -22,20 +22,20 @@ function check_scenario(f::Function, scenario::Scenario; verbose=true)
     try
         fv = f(trial)
     catch  e
-        @error "Parami: Objective function contains the following error:"
+        @error "HyperTuning: Objective function contains the following error:"
         println(e)
         return false
     end
     
     # TODO consider multi-objective
     if !trial.pruned && !(fv isa Number)
-        @error "Parami: Objective function must report a numerical value (except for pruned trials)."
+        @error "HyperTuning: Objective function must report a numerical value (except for pruned trials)."
         println("Value reported: ", fv)
         return false
     end
 
     if verbose
-        @info "Parami: Sampler and Objective function did not report errors."
+        @info "HyperTuning: Sampler and Objective function did not report errors."
         println("Evaluated trial: ")
         display(trial.values)
         println("")
